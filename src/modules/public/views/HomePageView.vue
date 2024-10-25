50186f002fe8ead6c5217ddceed742352c8af662
Cristian Campos <20213tn003@utez.edu.mx>
2024-10-25 14:15:34 -0600

    [ADD] recovery password views and initial router navigation

---
+++ src/modules/public/views/HomePageView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Public Home View Here</h1>
+    </div>
+  </template>
+  <script>
+  import Vue, { defineAsyncComponent } from "vue";
+  export default Vue.extend({
+    name: "HomePageView",
+  });
+  </script>
+  
