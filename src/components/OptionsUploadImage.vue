6a4d2909b0b0f449e5e7e12d8704c553078c00cf
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-10-29 01:43:08 -0600

    [UPDATE] takeImage

--- src/components/OptionsUploadImage.vue
+++ src/components/OptionsUploadImage.vue
@@ -21,46 +21,49 @@
             Foto</b-button
           >
         </b-col>
-        <b-col cols="12" md="12"> 
-        <b-card
-          v-if="showTakePicture === false && showUploadImage === false"
-          class="my-5 text-center"
-        >
-          <h3 class="text-grey">
-            <b-icon icon="arrow-up-circle"></b-icon>
-            Escoje una opción
-            <b-icon icon="arrow-up-circle"></b-icon>
-          </h3>
-        </b-card>
-        </b-col>
-        <b-col cols="12" md="12"> 
-        <b-card v-show="showTakePicture" class="my-2 text-center">
-          <Camera v-show="takePhoto === false" v-on:takePicture="takePicture" />
-          <Gallery v-show="takePhoto" />
-          <b-button
-            variant="red-palete"
-            v-show="takePhoto"
-            @click="takePicture"
-            class="my-2"
+        <b-col cols="12" md="12">
+          <b-card
+            v-if="showTakePicture === false && showUploadImage === false"
+            class="my-5 text-center"
           >
-            <b>Volver a tomar </b>
-          </b-button>
-          <b-button
-            variant="orange-secundary"
-            v-show="takePhoto"
-            @click="() => {}"
-            class="my-2 mx-2"
-          >
-            <b>Subir foto </b>
-          </b-button>
-          <!-- La imagen a descargar o cargar  -->
-          <img src="" />
-        </b-card>
+            <h3 class="text-grey">
+              <b-icon icon="arrow-up-circle"></b-icon>
+              Escoje una opción
+              <b-icon icon="arrow-up-circle"></b-icon>
+            </h3>
+          </b-card>
         </b-col>
         <b-col cols="12" md="12">
-        <b-card v-show="showUploadImage" class="my-2">
-          <DropZone />
-        </b-card>
+          <b-card v-show="showTakePicture" class="my-2 text-center">
+            <Camera
+              v-show="takePhoto === false && loading === false"
+              v-on:takePicture="takePicture"
+            />
+            <Gallery v-show="takePhoto && loading === false" />
+            <b-button
+              variant="red-palete"
+              v-show="takePhoto"
+              @click="takePicture"
+              class="my-2"
+            >
+              <b>Volver a tomar </b>
+            </b-button>
+            <b-button
+              variant="orange-secundary"
+              v-show="takePhoto"
+              @click="showLoading"
+              class="my-2 mx-2"
+            >
+              <b>Subir foto </b>
+            </b-button>
+            <b-progress :value="value" v-if="loading" class="mb-3"></b-progress>
+            <img :src="imageTakePhoto" v-show="loading === false" class="photo" height="200" />
+          </b-card>
+        </b-col>
+        <b-col cols="12" md="12">
+          <b-card v-show="showUploadImage" class="my-2">
+            <DropZone />
+          </b-card>
         </b-col>
       </b-row>
     </b-container>
@@ -87,6 +90,9 @@ export default Vue.extend({
       showUploadImage: false,
       showTakePicture: false,
       takePhoto: false,
+      imageTakePhoto: "",
+      loading: false,
+      value: 0,
     };
   },
   methods: {
@@ -109,11 +115,30 @@ export default Vue.extend({
           console.log(
             ctx.drawImage(video, 0, 0, picture.width, picture.height)
           );
+          // Convertir el contenido del canvas en una URL de base64
+          const imageDataUrl = picture.toDataURL("image/png");
+          this.imageTakePhoto = imageDataUrl;
+          console.log(imageDataUrl); // Muestra la URL de la imagen en consola
         }
         this.takePhoto = !this.takePhoto;
       } else {
         console.warn("Canvas or video element not found.");
       }
+    },
+    showLoading() {
+      this.loading = !this.loading;
+      this.loading = true;
+      this.value = 0;
+
+      const interval = 3000 / 100; // 3 segundos dividido entre 100 incrementos
+      const timer = setInterval(() => {
+        if (this.value < 100) {
+          this.value += 1;
+        } else {
+          clearInterval(timer);
+          this.loading = false;
+        }
+      }, interval);
     },
     showCameraOption() {
       this.showTakePicture = !this.showTakePicture;
@@ -134,5 +159,14 @@ export default Vue.extend({
 <style scoped>
 .text-grey {
   color: #abadaf;
+}
+
+.photo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
 }
 </style>
