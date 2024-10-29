eeed3b7b2bb6cf07c2ae15f8080475a32337cf34
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-10-29 00:50:16 -0600

    [ADD] photo-models

--- src/modules/auth/viewmodels/CreateAccountDelivery_ManViewModels.ts
+++ src/modules/auth/viewmodels/CreateAccountDelivery_ManViewModels.ts
@@ -1,58 +1,64 @@
 import Vue from "vue";
 
-
 export default Vue.extend({
-    setup(){
-        // se declara el vuevalidate
+  setup() {
+    // se declara el vuevalidate
+  },
+  data() {
+    return {
+      delivery_man: {
+        email: "",
+        passwod: "",
+        name: "",
+        first_last_name: "",
+        second_last_name: "",
+        face_photo: "",
+        front_identification_photo: "",
+        back_identification_photo: "",
+        address: {
+          city: "",
+          colony: "",
+          external_number: "",
+          internal_number: "",
+          locality: "",
+          reference_near: "", // referencia del lugar
+          state: "",
+          street: "",
+          zip_code: "", // codigo postal,
+          address_type: "",
+        },
+      },
+      currentStep: 1,
+      steps: [
+        "Datos personales",
+        "Identificación oficial (Frontal)",
+        "Identificación oficial (Trasera)",
+        "Foto Facial",
+      ],
+      password: "",
+      passwordVisible: false,
+      passwordVisibleConfirm: false,
+    };
+  },
+  methods: {
+    submitAccountForm() {
+      alert("Formulario enviado");
     },
-    data(){
-        return {
-            delivery_man:{
-                email:"",
-                passwod:"",
-                name:"",
-                first_last_name:"",
-                second_last_name:"",
-                address:{
-                    city:"",
-                    colony:"",
-                    external_number:"",
-                    internal_number:"",
-                    locality:"",
-                    reference_near:"",// referencia del lugar
-                    state:"",
-                    street:"",
-                    zip_code:"",// codigo postal,
-                    address_type:"",
-                }
-            },
-          currentStep: 1,
-          steps: ["Datos personales", "Identificación oficial (Frontal)","Identificación oficial (Trasera)","Foto Facial"],
-          password: "",
-          passwordVisible: false,
-          passwordVisibleConfirm: false,
-        };
-      },
-      methods: {
-        submitAccountForm() {
-          alert("Formulario enviado");
-        },
-        nextStep() {
-          if (this.currentStep < this.steps.length) {
-            this.currentStep++;
-          }
-        },
-        prevStep() {
-          if (this.currentStep > 1) {
-            this.currentStep--;
-          }
-        },
-        togglePasswordVisibility() {
-          this.passwordVisible = !this.passwordVisible;
-        },
-        togglePasswordVisibilityConfirm() {
-          this.passwordVisibleConfirm = !this.passwordVisibleConfirm;
-        }
-       
-      },
-})
+    nextStep() {
+      if (this.currentStep < this.steps.length) {
+        this.currentStep++;
+      }
+    },
+    prevStep() {
+      if (this.currentStep > 1) {
+        this.currentStep--;
+      }
+    },
+    togglePasswordVisibility() {
+      this.passwordVisible = !this.passwordVisible;
+    },
+    togglePasswordVisibilityConfirm() {
+      this.passwordVisibleConfirm = !this.passwordVisibleConfirm;
+    },
+  },
+});
