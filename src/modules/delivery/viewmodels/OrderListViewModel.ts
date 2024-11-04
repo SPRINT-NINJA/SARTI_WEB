c67cc0b7513ce1f49e997bc0df618b82a9d10963
AlanZoq3112 <20213tn009@utez.edu.mx>
2024-11-04 09:20:40 -0600

    [UPDATE] Modal de detalles del pedido

--- src/modules/delivery/viewmodels/OrderListViewModel.ts
+++ src/modules/delivery/viewmodels/OrderListViewModel.ts
@@ -8,8 +8,12 @@ export default defineComponent({
   },
   data() {
     return {
+      currentPage: 1, 
+      itemsPerPage: 4, 
+      selectedOrder: null,
       orders: [
         {
+          id: 1,
           name: "Producto 1",
           price: 100,
           description: "Este es el Producto 1",
@@ -18,6 +22,7 @@ export default defineComponent({
             "https://png.pngtree.com/png-clipart/20231116/original/pngtree-coca-cola-bottled-drink-isolated-photo-png-image_13575918.png",
         },
         {
+          id: 2,
           name: "Producto 2",
           price: 200,
           description: "Este es el Producto 2",
@@ -26,6 +31,7 @@ export default defineComponent({
             "https://png.pngtree.com/png-vector/20231018/ourmid/pngtree-pepsi-cola-bottles-isolated-on-white-with-reflection-png-image_10292878.png",
         },
         {
+          id: 3,
           name: "Producto 3",
           price: 150,
           description: "Este es el Producto 3",
@@ -34,6 +40,7 @@ export default defineComponent({
             "https://www.sampieri.com.mx/wp-content/uploads/2020/04/SPRITE-600-ml.jpg",
         },
         {
+          id: 4,
           name: "Producto 4",
           price: 300,
           description: "Este es el Producto 4",
@@ -42,6 +49,7 @@ export default defineComponent({
             "https://grupoelvalor.com/wp-content/uploads/2018/12/red-cola-600ml.png",
         },
         {
+          id: 5,
           name: "Producto 5",
           price: 400,
           description: "Este es el Producto 5",
@@ -49,6 +57,7 @@ export default defineComponent({
           image: "https://via.placeholder.com/150",
         },
         {
+          id: 6,
           name: "Producto 6",
           price: 250,
           description: "Este es el Producto 6",
@@ -63,7 +72,27 @@ export default defineComponent({
     getOrders() {
       console.log("Aqui seran los pedidos");
     },
+    
+    acceptOrder() {
+      console.log("Pedido aceptado")
+    },
+
+    declineOrder() {
+      console.log("Pedido rechazado")
+    },
+    
+    openOrderModal(orderId) {
+      this.selectedOrder = this.orders.find(order => order.id === orderId);
+      this.$bvModal.show("order-modal");
+    },
   },
+  computed: {
+    paginatedOrders() {
+        const start = (this.currentPage - 1) * this.itemsPerPage;
+        const end = start + this.itemsPerPage;
+        return this.orders.slice(start, end);
+    },
+},
   validations() {
     return {};
   },
--- src/modules/delivery/views/OrderList.vue
+++ src/modules/delivery/views/OrderList.vue
@@ -2,45 +2,60 @@
     <div>
         <navbar />
         <div class="container-fluid card-container">
+            <!-- Título de la sección -->
+            <h2 class="section-title">Pedidos disponibles</h2>
             <b-row class="justify-content-center mt-4">
-                <b-col cols="12" md="6" lg="6" class="mb-4" v-for="(order, index) in orders" :key="index">
+                <!-- Muestra solo los pedidos en la página actual -->
+                <b-col cols="12" md="6" lg="6" class="mb-4" v-for="(order, index) in paginatedOrders" :key="index">
                     <b-card class="h-100">
                         <b-row class="flex-column flex-md-row no-gutters">
                             <b-col cols="12" md="4" class="p-0">
-                                <b-img
-                                    :src="order.image"
-                                    class="object-cover img-size"
-                                    fluid
-                                    alt="Imagen del producto"
-                                ></b-img>
+                                <b-img :src="order.image" class="object-cover img-size" fluid
+                                    alt="Imagen del producto"></b-img>
                             </b-col>
-
                             <b-col cols="12" md="8" class="p-3">
                                 <h5 class="mb-2">{{ order.name }}</h5>
                                 <p><b>Precio:</b> ${{ order.price }}</p>
                                 <p><b>Descripción:</b> {{ order.description }}</p>
                                 <p><b>Usuario:</b> {{ order.userInfo.name }}</p>
                                 <p><b>Dirección:</b> {{ order.userInfo.address }}</p>
-                                
-                                <!-- Botones de acciones -->
+
                                 <div class="button-group mt-3">
-                                    <b-button variant="faded" class="mr-2 p-1">
+                                    <b-button variant="faded" class="mr-2 p-1" @click="openOrderModal(order.id)">
                                         <b-icon icon="eye"></b-icon>
                                     </b-button>
-                                    <b-button variant="success" class="mr-2">Aceptar Pedido</b-button>
-                                    <b-button variant="danger">Rechazar Pedido</b-button>
+                                    <b-button variant="red-palete" @click="declineOrder()">Rechazar Pedido</b-button>
+                                    <b-button variant="orange-primary" @click="acceptOrder()">Aceptar Pedido</b-button>
                                 </div>
                             </b-col>
                         </b-row>
                     </b-card>
                 </b-col>
             </b-row>
+            <div class="text-center">
+                <b-pagination v-model="currentPage" :total-rows="orders.length" :per-page="itemsPerPage"
+                    aria-controls="order-list" class="mt-4"></b-pagination>
+            </div>
+
+            <!-- Modal para mostrar detalles del pedido -->
+            <b-modal id="order-modal" title="Detalles del Pedido" v-if="selectedOrder" size="lg" centered>
+                <div class="modal-content-wrapper">
+                    <p><b>Producto:</b> {{ selectedOrder.name }}</p>
+                    <p><b>Precio:</b> ${{ selectedOrder.price }}</p>
+                    <p><b>Usuario:</b> {{ selectedOrder.userInfo.name }}</p>
+                    <p><b>Descripción:</b> {{ selectedOrder.description }}</p>
+                    <p><b>Dirección:</b> {{ selectedOrder.userInfo.address }}</p>
+                    <div class="image-center">
+                        <img :src="selectedOrder.image" alt="Imagen del producto" class="img-fluid img-card" />
+                    </div>
+                </div>
+            </b-modal>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Vue, { defineAsyncComponent } from "vue";
+import Vue from "vue";
 import OrderListViewModel from "../viewmodels/OrderListViewModel";
 export default {
     name: "DeliveryOrderList",
@@ -52,16 +67,39 @@ export default {
 </script>
 
 <style>
-.mt-4 {
+.img-card {
+    max-width: 350px;
+    max-height: 350px;
+}
+
+.image-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     margin-top: 1.5rem;
 }
 
-.mb-4 {
-    margin-bottom: 1.5rem;
+/* Estilo del título de la sección */
+.section-title {
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: left;
+    margin-bottom: 1rem;
+    margin-top: 20px;
 }
 
-.card-container {
-    margin: 20px;
+@media (max-width: 768px) {
+    .section-title {
+        font-size: 1.5rem;
+        margin-left: 15px;
+    }
+}
+
+@media (max-width: 576px) {
+    .section-title {
+        font-size: 1.2rem;
+        margin-left: 10px;
+    }
 }
 
 /* Estilo para la imagen */
@@ -88,23 +126,17 @@ b-card {
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
-}
-
-.button-group .mr-2 {
-    margin-right: 0.5rem;
+    gap: 0.5rem;
 }
 
 @media (max-width: 768px) {
     .img-size {
         max-height: 200px;
     }
+
     .button-group {
         flex-direction: column;
         align-items: stretch;
-    }
-    .button-group .mr-2 {
-        margin-right: 0;
-        margin-bottom: 0.5rem;
     }
 }
 
