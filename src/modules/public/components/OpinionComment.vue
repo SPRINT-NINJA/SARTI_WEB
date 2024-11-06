6c867a4056628138e317454984d6df7ef137fb53
Cristian Campos <i20213tn003@utez.edu.mx>
2024-11-05 21:16:51 -0600

    [ADD] details product view and components

---
+++ src/modules/public/components/OpinionComment.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div v-if="allImages">
+      <gallery-comments
+        :images="allImages"
+        galleryID="my-test-gallery"
+      ></gallery-comments>
+    </div>
+    <div v-for="(comment, index) in comments" :key="index">
+      <div class="h5 d-flex align-items-center">
+        <b-icon icon="person"></b-icon>
+        <span class="ml-2">{{ comment.username }}</span>
+      </div>
+      <div>
+        <b-row>
+          <b-col cols="6" md="6">
+            <b-form-rating
+              v-model="comment.rating"
+              stars="10"
+              readonly
+              no-border
+              no-focus
+              variant="warning"
+              size="sm"
+              class="m-0 p-0"
+            >
+            </b-form-rating>
+          </b-col>
+        </b-row>
+      </div>
+      <div>
+        <p>
+          {{ comment.comment }}
+        </p>
+      </div>
+      <div v-if="comment.images">
+        <gallery-comments
+          :images="comment.images"
+          galleryID="my-test-gallery"
+        ></gallery-comments>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  name: "OpinionComment",
+  props: {
+    images: Array,
+    comments: Array,
+  },
+  components: {
+    GalleryComments: defineAsyncComponent(() =>
+      import("../components/GalleryComments.vue")
+    ),
+  },
+  data() {
+    return {
+      allImages: [],
+    };
+  },
+  methods: {
+    mapImages() {
+      this.allImages = this.comments.flatMap((comment) => comment.images);
+    },
+  },
+  mounted() {
+    this.mapImages();
+    console.log(this.comments);
+  },
+};
+</script>
