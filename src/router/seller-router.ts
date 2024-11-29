f883a2ccbc0447936968fc88568ca76f2678314f
Cristian Campos <i20213tn003@utez.edu.mx>
2024-11-28 18:34:12 -0600

    [UPDATE] login view

--- src/router/seller-router.ts
+++ src/router/seller-router.ts
@@ -1,29 +1,32 @@
+const allowedRoles = ["EMPRENDEDOR"];
+
 export default [
-    {
-      path: "/seller",
-      component: () =>  import("../views/PublicView.vue"),
-      redirect: { name: "profile-seller" },
-      name: "seller",
-      children: [
-        {
-          path: "profile-seller",
-          name: "profile-seller",
-          component: () => import("../modules/auth/views/ProfileSellerView.vue"),
-          meta: {
-            title: "Perfil de emprendedor",
-            requireAuth: false,
-          },
+  {
+    path: "/seller",
+    component: () => import("../views/PublicView.vue"),
+    redirect: { name: "profile-seller" },
+    name: "seller",
+    children: [
+      {
+        path: "profile-seller",
+        name: "profile-seller",
+        component: () => import("../modules/auth/views/ProfileSellerView.vue"),
+        meta: {
+          title: "Perfil de emprendedor",
+          requireAuth: true,
+          role: allowedRoles,
         },
-        {
-            path: "rate-list",
-            name: "rate-list",
-            component: () => import("../modules/rates/views/RatesListView.vue"),
-            meta: {
-                title: "Lista de reseñas",
-                requireAuth: false,
-            },
-        }
+      },
+      {
+        path: "rate-list",
+        name: "rate-list",
+        component: () => import("../modules/rates/views/RatesListView.vue"),
+        meta: {
+          title: "Lista de reseñas",
+          requireAuth: true,
+          role: allowedRoles,
+        },
+      },
     ],
   },
 ];
-
