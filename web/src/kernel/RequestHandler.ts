3df339acf9f979e0a694c87e5263f7d737b4661e
Cristian Campos <i20213tn003@utez.edu.mx>
2024-12-06 04:21:30 -0600

    [ADD] some ts configuration reuestHandler and recovery password comsuption

---
+++ web/src/kernel/RequestHandler.ts
@@ -0,0 +1,24 @@
+import { CustomResponse } from "@/kernel/types";
+
+export class RequestHandler {
+  /**
+   * Maneja las promesas para las solicitudes HTTP y encapsula la respuesta en un CustomResponse.
+   * @param promise - La promesa resultante de una solicitud HTTP.
+   * @returns CustomResponse con los datos procesados o el error capturado.
+   */
+  public static async handleRequest<T>(promise: Promise<any>): Promise<CustomResponse<T>>{
+    try {
+      const response = await promise;
+      return response.data;
+    } catch (e:any) {
+      console.log("error", e);
+      return {
+        status: e.status,
+        statusMessage: e.data.status,
+        data: e.data?.data,
+        error: e.data?.error,
+        message: e.data?.message,
+      }
+    }
+  }
+}
