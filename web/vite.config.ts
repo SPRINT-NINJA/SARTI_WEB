2448888352f54b97b7a7bc169d10b8ea963baae8
PabloSFlores <i20213tn013@utez.edu.mx>
2024-12-10 02:19:17 -0600

    [UPDATE] vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/assets/', // Asegura que los archivos se sirvan desde /assets/
   plugins: [vue2()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   build: {
-    chunkSizeWarningLimit: 1600,
+    outDir: 'dist/assets', // Opcional: organiza el output dentro de una subcarpeta assets
   },
-})
+});
