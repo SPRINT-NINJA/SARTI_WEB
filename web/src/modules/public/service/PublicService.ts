32b7c1d642f5ebf335e254434a18ea7c370d0bdb
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2024-12-09 23:52:32 -0600

    [UPDATE] getproduct - sidebar

--- web/src/modules/public/service/PublicService.ts
+++ web/src/modules/public/service/PublicService.ts
@@ -1,11 +1,12 @@
-import { RequestHandler } from "@/kernel/RequestHandler";
-import { CustomResponse } from "../../../kernel/types";
 import axios from "../../../config/client.gateway";
 import { SellerListDto } from "../models/SellerListDto";
-import { SellerListModel } from "../models/SellerListModel";
+import { CustomResponse, CustomResponsePageable } from "@/kernel/types";
+import { RequestHandler } from "@/kernel/RequestHandler";
+import { IProduct } from "@/modules/products/models/ProductModel";
 
 export default class PublicService {
     private static baseUrl = "/seller";
+    private static baseUrlRate = "/rate";
 
     static async getSellers(payload: SellerListDto): Promise<CustomResponse<any>> {
         let pathParams = `?page=${payload.page}&size=${payload.size}&sort=${payload.sort},${payload.direction}`;
@@ -13,4 +14,17 @@ export default class PublicService {
             pathParams += `&bussinesName=${payload.businessName}`
         return await RequestHandler.handleRequest(axios.doGet(`${this.baseUrl}/all${pathParams}`));
     }
+
+
+    static async getResumeRateByProduct(payload: any): Promise<CustomResponse<any>> {
+        return await RequestHandler.handleRequest(axios.doGet(`${this.baseUrlRate}/resume/${payload.id}`));
+    }
+
+    static async getRateListByProduct(payload: any): Promise<CustomResponse<any>> {
+        return await RequestHandler.handleRequest(axios.doGet(`${this.baseUrlRate}/${payload.id}`));
+    }
+
+    
+
+
 }
--- web/src/modules/public/viewmodels/ProductListViewModel.ts
+++ web/src/modules/public/viewmodels/ProductListViewModel.ts
@@ -1,5 +1,8 @@
 import { encryptParamsId } from "@/kernel/utils/cryptojs";
 import { defineComponent } from "vue";
+import PublicService from "../service/PublicService";
+import { IProduct } from "@/modules/products/models/ProductModel";
+import ProductService from "@/modules/products/services/ProductService";
 
 export default defineComponent({
   data() {
@@ -34,6 +37,33 @@ export default defineComponent({
           rating: 8.5,
         },
       ],
+      optionsFieldToPage: [
+        { text: "5", value: 5 },
+        { text: "10", value: 10 },
+        { text: "20", value: 20 },
+        { text: "50", value: 50 },
+        { text: "100", value: 100 },
+      ],
+      productsPublic: [] as IProduct[],
+      pagedPayload: {
+        productName: "",
+        sellerId: "",
+        page: 1,
+        size: 5,
+        sort: "DESC",
+      },
+      isLoading: false,
+      pagination: {
+        page: 1,
+        sort: "id",
+        size: 5,
+        direction: "DESC",
+        totalRows: 0,
+        data: {
+          name: "",
+          hotelId: 0,
+        },
+      },
     };
   },
   methods: {
@@ -45,6 +75,32 @@ export default defineComponent({
       } catch (error) {
         console.error(error);
       }
+    },
+    async getAllProducts() {
+      try {
+        this.isLoading = true;
+        this.pagedPayload.page = this.pagination.page;
+        this.pagedPayload.size = this.pagination.size;
+        this.pagedPayload.sort = this.pagination.direction;
+        const resp = await ProductService.getAllProducts(this.pagedPayload);
+        const { error } = resp;
+        if (!error) {
+          this.productsPublic = resp.data.content;
+          this.pagination.totalRows = resp.data.totalElements;
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    async handlePageChange(page: number) {
+      this.pagination.page = page;
+      await this.getAllProducts();
+    },
+    async handleResetPage() {
+      this.pagination.page = 1;
+      await this.getAllProducts();
     },
     async getTopRatedProduct(){
       try {
@@ -60,5 +116,8 @@ export default defineComponent({
         console.error(error);
       }
     },
+  },
+  mounted() {
+    this.getAllProducts();
   },
 });
